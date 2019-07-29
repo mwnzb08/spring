d6cf3aeef76b5c5e01200c48b39dfaa7fd836300
mwnzb08 <moqiue0729@163.com>
2019-07-29 17:39:21 +0800

    add

---
+++ springbootgist/http-to-http/support-data/src/main/java/com/example/supportdata/SupportDataApplication.java
@@ -0,0 +1,15 @@
+package com.example.supportdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SupportDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportDataApplication.class, args);
+    }
+
+}
---
+++ springbootgist/http-to-http/support-data/src/test/java/com/example/supportdata/SupportDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.supportdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupportDataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

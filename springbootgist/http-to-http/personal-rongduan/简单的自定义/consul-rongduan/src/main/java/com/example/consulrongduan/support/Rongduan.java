d7da100569ed0da41ecd30f6043e82577a36ac4f
mwnzb08 <moqiue0729@163.com>
2019-07-31 22:07:50 +0800

    add data

--- springbootgist/http-to-http/personal-rongduan/简单的自定义/consul-rongduan/src/main/java/com/example/consulrongduan/support/Rongduan.java
+++ springbootgist/http-to-http/personal-rongduan/简单的自定义/consul-rongduan/src/main/java/com/example/consulrongduan/support/Rongduan.java
@@ -22,26 +22,26 @@ public class Rongduan {
     public Object doWithCircuitBreaker(ProceedingJoinPoint pjp) throws Throwable {
         String signature = pjp.getSignature().toLongString();//获取方法
         log.info("Invoke {}", signature);
-        Object retVal;
-        try {
-            if (counter.containsKey(signature)) {//有
-                if (counter.get(signature).get() > TIMES &&//判断值，当大于3进入
-                        breakCounter.get(signature).get() < TIMES) {
-                    log.warn("Circuit breaker return null, break {} times.",
-                            breakCounter.get(signature).incrementAndGet());
-                    return null;
+            Object retVal;
+            try {
+                if (counter.containsKey(signature)) {//有
+                    if (counter.get(signature).get() > TIMES &&//判断值，当大于3进入
+                            breakCounter.get(signature).get() < TIMES) {
+                        log.warn("Circuit breaker return null, break {} times.",
+                                breakCounter.get(signature).incrementAndGet());
+                        return null;
+                    }
+                } else {
+                    counter.put(signature, new AtomicInteger(0));//没有就设置为0
+                    breakCounter.put(signature, new AtomicInteger(0));
                 }
-            } else {
-                counter.put(signature, new AtomicInteger(0));//没有就设置为0
-                breakCounter.put(signature, new AtomicInteger(0));
-            }
-            retVal = pjp.proceed();//获取实
-            counter.get(signature).set(0);
-            breakCounter.get(signature).set(0);
-        } catch (Throwable t) {
-            log.warn("Circuit breaker counter: {}, Throwable {}",
-                    counter.get(signature).incrementAndGet(), t.getMessage());//失败+1
-            breakCounter.get(signature).set(0);
+                retVal = pjp.proceed();//获取实
+                counter.get(signature).set(0);
+                breakCounter.get(signature).set(0);
+            } catch (Throwable t) {
+                log.warn("Circuit breaker counter: {}, Throwable {}",
+                        counter.get(signature).incrementAndGet(), t.getMessage());//失败+1
+                breakCounter.get(signature).set(0);
             throw t;
         }
         return retVal;
